30cf5dd769f6cd8870afb6fed259633c28d71091
Wei Li <wei.li@feedhenry.com>
2014-04-10 15:55:43 +0100

    adding xamarin ios and android sdk projects

---
+++ FHXamarinAndroidSDK/Services/DeviceService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FHSDK.Services
+{
+	public class DeviceService : IDeviceService
+	{
+		public DeviceService  ()
+		{
+		}
+
+		public DeviceService ()
+		{
+		}
+
+		public String GetDeviceId()
+		{
+			return null;
+		}
+
+		public string GetData(string dataId)
+		{
+			return null;
+		}
+
+		public void SaveData(string dataId, string dataValue)
+		{
+		}
+
+		public Task<string> ReadResourceAsString(string resourceName)
+		{
+			return null;
+		}
+	}
+}
+
---
+++ FHXamarinIOSSDK/Services/DeviceService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FHSDK.Services
+{
+	public class DeviceService : IDeviceService
+	{
+		public DeviceService ()
+		{
+		}
+
+		public String GetDeviceId()
+		{
+			return null;
+		}
+
+		public string GetData(string dataId)
+		{
+			return null;
+		}
+
+		public void SaveData(string dataId, string dataValue)
+		{
+		}
+
+		public Task<string> ReadResourceAsString(string resourceName)
+		{
+			return null;
+		}
+
+	}
+}
+
