6f5b2165942ad42693f1a2d151eba5a92b860212
Erik Jan de Wit <erikjan.dewit@gmail.com>
2015-10-23 17:31:37 +0200

    try with xUnit

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,19 +1,18 @@
 ﻿using System.Threading.Tasks;
 using FHSDK;
 using FHSDKPortable;
-using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Xunit;
 
 namespace tests
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+        [Fact]
         public async Task TestMethod1()
         {
             await FHClient.Init();
             var host = FH.GetCloudHost();
-            Assert.IsTrue(host.Contains("http://192.168.28.34:8001"));
+            Assert.True(host.Contains("http://192.168.28.34:8001"));
 
         }
     }
