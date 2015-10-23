c1c9796b17b0c001b8114425bb955f2d58be3371
Erik Jan de Wit <erikjan.dewit@gmail.com>
2015-10-23 16:32:56 +0200

    windows 8.1 unit test

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using FHSDK;
+using FHSDKPortable;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            await FHClient.Init();
+            var host = FH.GetCloudHost();
+            Assert.IsTrue(host.Contains("http://192.168.28.34:8001"));
+
+        }
+    }
+}
