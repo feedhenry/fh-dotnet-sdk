6809ea69cade0fe02315f9b2eff6ae7962969067
Erik Jan de Wit <erikjan.dewit@gmail.com>
2015-10-27 14:55:28 +0100

    more tests

---
+++ tests/TaskModel.cs
@@ -0,0 +1,22 @@
+﻿using FHSDK.Sync;
+using Newtonsoft.Json;
+
+namespace FHSDKTestShared
+{
+    public class TaskModel : IFHSyncModel
+    {
+        [JsonProperty("taskName")]
+        public string TaksName { set; get; }
+
+        [JsonProperty("completed")]
+        public bool Completed { set; get; }
+
+        [JsonIgnore]
+        public string UID { set; get; }
+
+        public override string ToString()
+        {
+            return string.Format("[TaskModel: UID={0}, TaksName={1}, Completed={2}]", UID, TaksName, Completed);
+        }
+    }
+}
--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,5 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using FHSDK;
+using FHSDK.Services;
+using FHSDK.Services.Device;
+using FHSDK.Services.Hash;
+using FHSDK.Sync;
 using FHSDKPortable;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 
@@ -9,12 +15,16 @@ namespace tests
     public class UnitTest1
     {
         [TestMethod]
-        public async Task TestMethod1()
+        public async Task TestInit()
         {
+            //given
             await FHClient.Init();
-            var host = FH.GetCloudHost();
-            Assert.IsTrue(host.Contains("http://192.168.28.34:8001"));
 
+            //when
+            var host = FH.GetCloudHost();
+
+            //then
+            Assert.True(host.Contains("http://192.168.28.34:8001"));
         }
     }
 }
