ea0cc6251aeb65eaab5d2b0eb4571d81c8026510
corinnerkych <corinnekrych@gmail.com>
2015-10-30 15:04:55 +0100

    ginfig test

---
+++ tests/FHConfigTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using FHSDK.Config;
+
+namespace tests
+{
+    class FHConfigTest
+    {
+        [Fact]
+        public async Task TestReadConfig()
+        {
+            // given
+            var config = FHConfig.GetInstance();
+            
+            // when
+            // default instanciation
+
+            // then
+            Assert.Equal("a94a8fe5ccb19ba61c4c0873d391e987982fbbd3", config.GetHost());
+        }
+    }
+}
