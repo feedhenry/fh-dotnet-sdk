ada8956b78f14d0ad8a0b8a77ba6f399166dd810
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-03-24 11:30:45 +0100

    now uses xamarin os instead of mono touch

--- copyIOSLibs.sh
+++ copyIOSLibs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp -f ./xamarin/FHXamarinIOSSDK/bin/Release/FHSDK.dll ./Dist/lib/monotouch/
+cp -f ./xamarin/FHXamarinIOSSDK/bin/Release/FHSDK.dll ./Dist/lib/xamarinios10/
 #cp -f ./xamarin/FHXamarinIOSSDK/bin/Release/FHSDK.xml ./Dist/lib/monotouch/
-cp -f ./xamarin/FHXamarinIOSSDK/bin/Release/FHXamarinIOSSDK.dll ./Dist/lib/monotouch/
-cp -f ./xamarin/FHXamarinIOSSDK/bin/Release/FHXamarinIOSSDK.XML ./Dist/lib/monotouch/
+cp -f ./xamarin/FHXamarinIOSSDK/bin/Release/FHXamarinIOSSDK.dll ./Dist/lib/xamarinios10/
+cp -f ./xamarin/FHXamarinIOSSDK/bin/Release/FHXamarinIOSSDK.XML ./Dist/lib/xamarinios10/
