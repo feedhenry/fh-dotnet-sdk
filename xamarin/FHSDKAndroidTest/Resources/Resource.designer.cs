ac7023e6b907a480be2fb840d7635365da5ef7a6
Hoyt Summers Pittman <secondsun@gmail.com>
2016-05-25 11:56:59 -0400

    Updating Android and Windows binary libraries

--- xamarin/FHSDKAndroidTest/Resources/Resource.designer.cs
+++ xamarin/FHSDKAndroidTest/Resources/Resource.designer.cs
@@ -197,6 +197,114 @@ namespace FHSDKAndroidTest
 			{
 			}
 		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050001
+			public const int OptionHostName = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int OptionPort = 2131034114;
+			
+			// aapt resource value: 0x7f050000
+			public const int OptionRemoteServer = 2131034112;
+			
+			// aapt resource value: 0x7f050010
+			public const int OptionsButton = 2131034128;
+			
+			// aapt resource value: 0x7f05000b
+			public const int ResultFullName = 2131034123;
+			
+			// aapt resource value: 0x7f05000d
+			public const int ResultMessage = 2131034125;
+			
+			// aapt resource value: 0x7f05000c
+			public const int ResultResultState = 2131034124;
+			
+			// aapt resource value: 0x7f05000a
+			public const int ResultRunSingleMethodTest = 2131034122;
+			
+			// aapt resource value: 0x7f05000e
+			public const int ResultStackTrace = 2131034126;
+			
+			// aapt resource value: 0x7f050006
+			public const int ResultsFailed = 2131034118;
+			
+			// aapt resource value: 0x7f050003
+			public const int ResultsId = 2131034115;
+			
+			// aapt resource value: 0x7f050007
+			public const int ResultsIgnored = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int ResultsInconclusive = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int ResultsMessage = 2131034121;
+			
+			// aapt resource value: 0x7f050005
+			public const int ResultsPassed = 2131034117;
+			
+			// aapt resource value: 0x7f050004
+			public const int ResultsResult = 2131034116;
+			
+			// aapt resource value: 0x7f05000f
+			public const int RunTestsButton = 2131034127;
+			
+			// aapt resource value: 0x7f050011
+			public const int TestSuiteListView = 2131034129;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
+		public partial class Layout
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int options = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int results = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int test_result = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int test_suite = 2130903043;
+			
+			static Layout()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Layout()
+			{
+			}
+		}
+		
+		public partial class String
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int library_name = 2130968576;
+			
+			static String()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private String()
+			{
+			}
+		}
 	}
 }
 #pragma warning restore 1591
