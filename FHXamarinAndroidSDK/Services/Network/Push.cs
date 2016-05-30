6d63e51718cd77e09ee20813f9430b133d6b6696
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-05-30 16:01:23 +0200

    move projects

---
+++ FHXamarinAndroidSDK/Services/Network/Push.cs
@@ -0,0 +1,13 @@
+﻿using AeroGear.Push;
+using FHSDK.Services.Network;
+
+namespace FHSDK.Services
+{
+    public class Push : PushBase
+    {
+        protected override Registration CreateRegistration()
+        {
+            return new GcmRegistration();
+        }
+    }
+}
