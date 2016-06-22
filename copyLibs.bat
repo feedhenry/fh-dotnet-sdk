2191d65f35700d286dc345e07b07c9bfe8a9b49b
Hoyt Summers Pittman <secondsun@gmail.com>
2016-06-22 11:45:37 -0400

    Adding sources to nuget

--- copyLibs.bat
+++ copyLibs.bat
@@ -19,10 +19,25 @@ COPY /B FHSDKPortable\bin\Release\FHSDKPortable.XML Dist\lib\wpa81\
 COPY /B FHSDKPortable\bin\Release\FHSDKPortable.dll "Dist\lib\portable-win81+wpa81\"
 COPY /B FHSDKPortable\bin\Release\FHSDKPortable.XML "Dist\lib\portable-win81+wpa81\"
 
-COPY /B xamarin\FHXamarinAndroidSDK\bin\Release\FHSDK.dll Dist\lib\monoandroid\
-COPY /B xamarin\FHXamarinAndroidSDK\bin\Release\FHSDK.xml Dist\lib\monoandroid\
-COPY /B xamarin\FHXamarinAndroidSDK\bin\Release\FHXamarinAndroidSDK.dll Dist\lib\monoandroid\
-COPY /B xamarin\FHXamarinAndroidSDK\bin\Release\FHXamarinAndroidSDK.xml Dist\lib\monoandroid\
+COPY /B FHXamarinAndroidSDK\bin\Release\FHSDK.dll Dist\lib\monoandroid\
+COPY /B FHXamarinAndroidSDK\bin\Release\FHSDK.xml Dist\lib\monoandroid\
+COPY /B FHXamarinAndroidSDK\bin\Release\FHXamarinAndroidSDK.dll Dist\lib\monoandroid\
+COPY /B FHXamarinAndroidSDK\bin\Release\FHXamarinAndroidSDK.xml Dist\lib\monoandroid\
+
+REM copy source
+IF not exist "Dist\src\FHSDK\" (md "Dist\src\FHSDK\")
+IF not exist "Dist\src\FHSDKPhone\" (md "Dist\src\FHSDKPhone\")
+IF not exist "Dist\src\FHSDKPortable\" (md "Dist\src\FHSDKPortable\")
+IF not exist "Dist\src\FHXamarinAndroidSDK\" (md "Dist\src\FHXamarinAndroidSDK\")
+IF not exist "Dist\src\FHXamarinIOSSDK\" (md "Dist\src\FHXamarinIOSSDK\")
+
+
+
+XCOPY FHSDK\*.cs Dist\src\FHSDK /sy
+XCOPY FHSDKPhone\*.cs Dist\src\FHSDKPhone /sy
+XCOPY FHSDKPortable\*.cs Dist\src\FHSDKPortable /sy
+XCOPY FHXamarinAndroidSDK\*.cs Dist\src\FHXamarinAndroidSDK /sy
+XCOPY FHXamarinIOSSDK\*.cs Dist\src\FHXamarinIOSSDK /sy
 
 REM COPY /B xamarin\FHXamarinIOSSDK\bin\Release\FHSDK.dll Dist\lib\monotouch\
 REM COPY /B xamarin\FHXamarinIOSSDK\bin\Release\FHSDK.xml Dist\lib\monotouch\
