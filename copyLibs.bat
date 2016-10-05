12fff5c6c53d8bb48ec5d63c995880a85846a49e
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-10-05 16:14:03 +0200

    fix the release process

--- copyLibs.bat
+++ copyLibs.bat
@@ -1,28 +1,20 @@
-COPY /B FHSDK\bin\Release\FHSDK.dll "Dist\lib\portable-net45+wp80+wp81+win8+wpa81\"
-COPY /B FHSDK\bin\Release\FHSDK.xml "Dist\lib\portable-net45+wp80+wp81+win8+wpa81\"
+COPY /B FHSDK\bin\Debug\FHSDK.dll "Dist\lib\portable-net45+win+wpa81+wp80+MonoAndroid10+xamarinios10+MonoTouch10\"
+COPY /B FHSDK\bin\Debug\FHSDK.xml "Dist\lib\portable-net45+win+wpa81+wp80+MonoAndroid10+xamarinios10+MonoTouch10\"
 
-COPY /B FHSDKPhone\Bin\Release\FHSDK.dll Dist\lib\wp80\
-COPY /B FHSDKPhone\Bin\Release\FHSDK.xml Dist\lib\wp80\
-COPY /B FHSDKPhone\Bin\Release\FHSDKPhone.dll Dist\lib\wp80\
-COPY /B FHSDKPhone\Bin\Release\FHSDKPhone.xml Dist\lib\wp80\
+COPY /B FHSDKPhone\Bin\Debug\FHSDK.dll Dist\lib\wp80\
+COPY /B FHSDKPhone\Bin\Debug\FHSDK.xml Dist\lib\wp80\
+COPY /B FHSDKPhone\Bin\Debug\FHSDKPhone.dll Dist\lib\wp80\
+COPY /B FHSDKPhone\Bin\Debug\FHSDKPhone.xml Dist\lib\wp80\
 
-COPY /B FHSDKPortable\bin\Release\FHSDK.dll Dist\lib\wp81\
-COPY /B FHSDKPortable\bin\Release\FHSDK.xml Dist\lib\wp81\
-COPY /B FHSDKPortable\Bin\Release\FHSDKPortable.dll Dist\lib\wp81\
-COPY /B FHSDKPortable\Bin\Release\FHSDKPortable.xml Dist\lib\wp81\
+COPY /B FHSDKPortable\bin\Debug\FHSDK.dll "Dist\lib\portable-win81+wpa81\"
+COPY /B FHSDKPortable\bin\Debug\FHSDK.XML "Dist\lib\portable-win81+wpa81\"
+COPY /B FHSDKPortable\bin\Debug\FHSDKPortable.dll "Dist\lib\portable-win81+wpa81\"
+COPY /B FHSDKPortable\bin\Debug\FHSDKPortable.XML "Dist\lib\portable-win81+wpa81\"
 
-COPY /B FHSDKPortable\bin\Release\FHSDK.dll Dist\lib\wpa81\
-COPY /B FHSDKPortable\bin\Release\FHSDK.xml Dist\lib\wpa81\
-COPY /B FHSDKPortable\bin\Release\FHSDKPortable.dll Dist\lib\wpa81\
-COPY /B FHSDKPortable\bin\Release\FHSDKPortable.XML Dist\lib\wpa81\
-
-COPY /B FHSDKPortable\bin\Release\FHSDKPortable.dll "Dist\lib\portable-win81+wpa81\"
-COPY /B FHSDKPortable\bin\Release\FHSDKPortable.XML "Dist\lib\portable-win81+wpa81\"
-
-COPY /B FHXamarinAndroidSDK\bin\Release\FHSDK.dll Dist\lib\monoandroid\
-COPY /B FHXamarinAndroidSDK\bin\Release\FHSDK.xml Dist\lib\monoandroid\
-COPY /B FHXamarinAndroidSDK\bin\Release\FHXamarinAndroidSDK.dll Dist\lib\monoandroid\
-COPY /B FHXamarinAndroidSDK\bin\Release\FHXamarinAndroidSDK.xml Dist\lib\monoandroid\
+COPY /B FHXamarinAndroidSDK\bin\Debug\FHSDK.dll Dist\lib\monoandroid\
+COPY /B FHXamarinAndroidSDK\bin\Debug\FHSDK.xml Dist\lib\monoandroid\
+COPY /B FHXamarinAndroidSDK\bin\Debug\FHXamarinAndroidSDK.dll Dist\lib\monoandroid\
+COPY /B FHXamarinAndroidSDK\bin\Debug\FHXamarinAndroidSDK.xml Dist\lib\monoandroid\
 
 REM copy source
 IF not exist "Dist\src\FHSDK\" (md "Dist\src\FHSDK\")
@@ -39,7 +31,7 @@ XCOPY FHSDKPortable\*.cs Dist\src\FHSDKPortable /sy
 XCOPY FHXamarinAndroidSDK\*.cs Dist\src\FHXamarinAndroidSDK /sy
 XCOPY FHXamarinIOSSDK\*.cs Dist\src\FHXamarinIOSSDK /sy
 
-REM COPY /B xamarin\FHXamarinIOSSDK\bin\Release\FHSDK.dll Dist\lib\monotouch\
-REM COPY /B xamarin\FHXamarinIOSSDK\bin\Release\FHSDK.xml Dist\lib\monotouch\
-REM COPY /B xamarin\FHXamarinIOSSDK\bin\Release\FHXamarinIOSSDK.dll Dist\lib\monotouch\
-REM COPY /B xamarin\FHXamarinIOSSDK\bin\Release\FHXamarinIOSSDK.xml Dist\lib\monotouch\
+REM COPY /B xamarin\FHXamarinIOSSDK\bin\Debug\FHSDK.dll Dist\lib\monotouch\
+REM COPY /B xamarin\FHXamarinIOSSDK\bin\Debug\FHSDK.xml Dist\lib\monotouch\
+REM COPY /B xamarin\FHXamarinIOSSDK\bin\Debug\FHXamarinIOSSDK.dll Dist\lib\monotouch\
+REM COPY /B xamarin\FHXamarinIOSSDK\bin\Debug\FHXamarinIOSSDK.xml Dist\lib\monotouch\
