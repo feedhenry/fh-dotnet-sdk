9d3a8cfed466cbda1447572af986ad9664a9ce96
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-10-06 08:28:18 +0200

    fixed folder

--- copyIOSLibs.sh
+++ copyIOSLibs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cp -f ./xamarin/FHXamarinIOSSDK/bin/Debug/FHSDK.dll ./Dist/lib/xamarinios10/
-cp -f ./xamarin/FHXamarinIOSSDK/bin/Debug/FHXamarinIOSSDK.dll ./Dist/lib/xamarinios10/
-cp -f ./xamarin/FHXamarinIOSSDK/bin/Debug/FHXamarinIOSSDK.XML ./Dist/lib/xamarinios10/
+cp -f ./FHXamarinIOSSDK/bin/Debug/FHSDK.dll ./Dist/lib/xamarinios10/
+cp -f ./FHXamarinIOSSDK/bin/Debug/FHXamarinIOSSDK.dll ./Dist/lib/xamarinios10/
+cp -f ./FHXamarinIOSSDK/bin/Debug/FHXamarinIOSSDK.XML ./Dist/lib/xamarinios10/
