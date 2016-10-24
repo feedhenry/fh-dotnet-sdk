a62147a3b2fff6549f30cf797f652fed240be240
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-10-24 17:36:41 +0200

    deserialse to domain class rirght from the response

---
+++ tests/ResponseTest.cs
@@ -0,0 +1,38 @@
+﻿#if __MOBILE__
+using Xunit;
+using TestMethod = Xunit.FactAttribute;
+#else
+using System.Net;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+#endif
+using System.Collections.Generic;
+using System.Net;
+using FHSDK;
+using Newtonsoft.Json;
+
+namespace tests
+{
+    [TestClass]
+    public class ResponseTest
+    {
+        [TestMethod]
+        public void DeserialseResponseTest()
+        {
+            //given
+            var response = new FHResponse(HttpStatusCode.OK, "[{ \"name\": \"test\" }, { \"name\": \"erik\" }]");
+
+            //when
+            var result = response.GetResponseAs<List<Person>>();
+
+            //then
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result[0].Name, "test");
+        }
+    }
+
+    internal class Person
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
